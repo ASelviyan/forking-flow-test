d73d090bfcb17dedf7c60d57c66c126018416297
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-06-23 16:34:44 -0500

    adds bio to carlos

--- src/Carlos.js
+++ src/Carlos.js
@@ -5,6 +5,8 @@ export default function Carlos() {
     <div>
         <h1>Carlos</h1>
         <img src='https://wl-brightside.cf.tsp.li/resize/728x/jpg/e54/e78/30a24451a8ba30d37a3dfa888c.jpg' alt='funny cat pic' />
+
+        <p>This is a bio about myself.</p>
     </div>
   )
 }
